14e5e6e13891c8d9415f49868a9d862eb8fe1cf7
Frankz <leninalarcon7@gmail.com>
2022-02-25 19:49:30 -0500

    preparando deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {getData, getOwnerData} = require('./src/getTwitterData');
 const cors = require('cors');
 const app = express();
-const port = 3100;
+const port = 3000;
 
 app.use(express.json())
 app.use(cors());
